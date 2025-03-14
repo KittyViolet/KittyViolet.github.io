006a1ef5ecb7bfadbf9fbd1178eb5b4c4a7061dd
Violet <seandinkle69@gmail.com>
2025-03-14 12:29:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const aboutMeModal = document.getElementById("aboutMeModal");
 const closeModal = document.getElementById("closeModal");
 
 aboutMeButton.addEventListener("click", function () {
-  aboutMeModal.style.display = "block";
+  aboutMeModal.style.display = "block"; // Show the modal
 });
 
 closeModal.addEventListener("click", function () {
-  aboutMeModal.style.display = "none";
+  aboutMeModal.style.display = "none"; // Hide the modal
 });
 
 // Close the modal when clicking anywhere outside of it
 window.addEventListener("click", function (event) {
   if (event.target === aboutMeModal) {
-    aboutMeModal.style.display = "none";
+    aboutMeModal.style.display = "none"; // Hide the modal
   }
 });
